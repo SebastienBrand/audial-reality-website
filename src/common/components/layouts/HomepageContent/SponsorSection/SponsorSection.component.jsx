6c005e0ffb3685f21669554ea278ce5ae32d7f51
Sheldon Jackson <Sheldon.ej@gmail.com>
2023-02-28 09:50:23 -0500

    Starting the Website
    Making the basic template so that the developers can have something to work with!

---
+++ src/common/components/layouts/HomepageContent/SponsorSection/SponsorSection.component.jsx
@@ -0,0 +1,22 @@
+import styles from './SponsorSection.module.css'
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+import Image1 from '@/images/location.png';
+
+
+import { BsChevronDoubleDown } from 'react-icons/bs'
+
+
+
+export default function SponsorSection( props ){
+     return ( 
+        <div className={`flex flow-row ${styles['spnosor-section-container']}`}>
+           { /* DEV-TASK [Hard-Difficult]: Make a marquee animation for the sponsors. To do so get a list of images and make 
+                them greyscale with filter and grey scale properties. Then make them slowly scroll left to right across the screen in this container.
+                Making the looping seemless so it looks as if it is never ending. This can also be done with a pop and push method as well. */}
+        </div>
+    )
+    
+}
