3d72ffee273efb60de800c4c89d477013f89a5c6
sMorrison26 <morris10@rpi.edu>
2023-03-31 18:46:21 -0400

    init completion page
    Implementing a submission page that loads when email is successfully signed up.

---
+++ src/pages/coming-soon-complete/index.js
@@ -0,0 +1,21 @@
+
+export default function comingSoonComplete() {
+
+    return (
+        <div className="thank-you-page"> 
+            <div className="top">
+                <h1>Soundscape</h1>
+                <p>for everyone</p>    
+            </div>
+            
+            
+            <div className="bottom">
+                <p>You're in, thanks!</p>
+                <p>Thanks for subscribing - You will recieve product <br />
+                    updates directly in your inbox.
+                </p>
+            </div>
+        </div>
+    );
+
+}
